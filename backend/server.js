f7b5979be0d0cdc852623927abdad1985207fb6d
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2018-10-21 21:48:12 +0530

    created a post service

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var messages = [{text:'some text', owner:'owner1'},{text:'changed text', owner:'owner2'},{text:'some text', owner:'owner3'}];
-app.use((re, res, next) => {
+app.use(bodyParser.json());
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" );
     next();
 })
 app.get('/messages', (req, res) => {
     res.json(messages);
+})
+
+app.post('/messages',(req, res) => {
+    console.log(req.body)
+    res.sendStatus(200)
 })
 
 app.listen(1234);
